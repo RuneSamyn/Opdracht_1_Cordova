36299eb36651daab33a096e02ac50ee3d139f231
rune samyn <rune.samyn@gmail.com>
2020-10-08 09:56:24 +0200

    add bootstrap to project

--- www/js/index.js
+++ www/js/index.js
@@ -26,18 +26,19 @@ function onDeviceReady() {
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     console.log(navigator.camera);
+    document.getElementById("cameraBtn").addEventListener("click", openCamera);
 }
 
 function setOptions(srcType) {
     var options = {
         // Some common settings are 20, 50, and 100
-        quality: 50,
+        quality: 100,
         destinationType: Camera.DestinationType.FILE_URI,
         // In this app, dynamically set the picture source, Camera or photo gallery
         sourceType: srcType,
         encodingType: Camera.EncodingType.JPEG,
         mediaType: Camera.MediaType.PICTURE,
-        allowEdit: true,
+        //allowEdit: true,
         correctOrientation: true  //Corrects Android orientation quirks
     }
     return options;
@@ -52,8 +53,6 @@ function openCamera() {
     navigator.camera.getPicture(function cameraSuccess(imageUri) {
 
         displayImage(imageUri);
-        // You may choose to copy the picture, save it somewhere, or upload.
-        func(imageUri);
 
     }, function cameraError(error) {
         console.debug("Unable to obtain picture: " + error, "app");
@@ -62,7 +61,24 @@ function openCamera() {
 }
 
 function displayImage(imgUri) {
+    var foto = document.getElementById('imageCard');
+    foto.src = imgUri;
+    var text = document.getElementById('imageUrl');
+    text.innerText = imgUri;
+}
 
-    var elem = document.getElementById('imageFile');
-    elem.src = imgUri;
+function createNewFileEntry(imgUri) {
+    window.resolveLocalFileSystemURL(cordova.file.cacheDirectory, function success(dirEntry) {
+
+        // JPEG file
+        dirEntry.getFile("tempFile.jpeg", { create: true, exclusive: false }, function (fileEntry) {
+
+            // Do something with it, like write to it, upload it, etc.
+            // writeFile(fileEntry, imgUri);
+            console.log("got file: " + fileEntry.fullPath);
+            // displayFileData(fileEntry.fullPath, "File copied to");
+
+        }, onErrorCreateFile);
+
+    }, onErrorResolveUrl);
 }
